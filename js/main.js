0a3316da3c7b51f3fe7478d797e12439cf66f4f3
davidRamos <109716289+craken010@users.noreply.github.com>
2026-01-27 15:07:08 -0500

    feat: manejo de errores al consumir productos

--- js/main.js
+++ js/main.js
@@ -1,19 +1,37 @@
 const contenedorProductos = document.getElementById("productos");
 const cargandoTexto = document.getElementById("cargando")
+
 fetch("https://fakestoreapi.com/products")
-  .then(respuesta => respuesta.json())
+  .then(respuesta => {
+    if (!respuesta.ok) {
+      throw new Error("Error al conectar con la API");
+    }
+    return respuesta.json();
+  })
   .then(productos => {
-      cargandoTexto.style.display = "none";
+    cargandoTexto.style.display = "none";
+
     productos.forEach(producto => {
       const article = document.createElement("article");
       article.classList.add("producto");
+
       article.innerHTML = `
         <img src="${producto.image}" alt="${producto.title}">
         <h2 class="titulo">${producto.title}</h2>
-        <p class="precio">$${producto.price}</p>`;
-        
+        <p class="precio">$${producto.price}</p>
+        <p class="categoria"${producto.category}</p}`;
+
       contenedorProductos.appendChild(article);
+
     });
+  })
+  .catch(error => {
+    cargandoTexto.textContent = "Error al cargar los productos";
+    console.error(error);
   });
+
+
+
+
 
 
